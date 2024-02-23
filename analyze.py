7d8465bcf82b39ebe0506109dc7506be1cb428e5
palewire <b@palewi.re>
2024-02-23 17:27:43 -0500

    Add monthly run

--- analyze.py
+++ analyze.py
@@ -10,9 +10,14 @@ from rich import print
 THIS_DIR = Path(__file__).parent
 
 
-@click.command()
+@click.group()
 def cli():
     """Analyze our data."""
+    pass
+
+
+@cli.command()
+def org_report():
     # Load the data
     repo_df = pd.read_csv(THIS_DIR / "repos.csv", parse_dates=["updated_at"])
 
@@ -28,6 +33,25 @@ def cli():
 
     # Write out the data
     org_df.to_csv(THIS_DIR / "org-report.csv")
+
+
+@cli.command()
+def new_repos_by_month():
+    """Output the number of new repos by month."""
+    # Load the data
+    repo_df = pd.read_csv(THIS_DIR / "repos.csv", parse_dates=["created_at"])
+
+    # Group by the organization, count the number of repos and list the latest update
+    org_df = repo_df.groupby(
+        pd.Grouper(key="created_at", freq="ME")
+    ).size().reset_index(name="count")
+
+    # Convert the date to a YYYY-MM format
+    org_df["created_at"] = org_df["created_at"].dt.strftime("%Y-%m")
+
+    # Write out the data
+    org_df.to_csv(THIS_DIR / "new-repos-by-month.csv", index=False)
+
 
 
 if __name__ == "__main__":
